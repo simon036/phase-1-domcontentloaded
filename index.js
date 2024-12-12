e36074501300b4da5c2c332a1684fa9c8ca33d85
Simon <ndungusimon564@gmail.com>
2024-12-12 21:53:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener('DOMContentLoaded', function() {
-    var paragraph = document.getElementById('text');
+    const paragraph = document.getElementById('text');
     paragraph.textContent = 'This is really cool!';
   });
